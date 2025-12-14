29cf3e83fdb145da7c23a30454eea9faf5407aba
sorin <sorinciupitu@gmail.com>
2025-12-14 13:42:41 +0200

    Resolve .gitignore merge conflict; ignore backend persistent dirs

---
+++ trezorerie-app/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3005, // Port dev implicit
+    strictPort: true,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5001',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  }
+})
